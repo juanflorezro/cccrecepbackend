328fe4957793b21fd247687586d259fb5994ee79
juanflorezro <142103189+juanflorezro@users.noreply.github.com>
2023-09-11 13:40:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const corsOptions = {
-  origin: ['https://cccrecep.juanflow04flore.repl.co','https://cccrecep.juanflow04flore.repl.co/main/entradaPersona'],
-  headers: "*", 
-  methods: "*"
-}
 const bodyParser = require('body-parser')
 const loginUsuarioRouter = require('./routes/login/login')
 const {main,storageSize} = require('./connection/connection')
 const gestionUsuariosRouter = require('./routes/gestion/gestionUsuarios')
 const gestionEntradasRouter = require('./routes/gestion/gestionEntradas')
 const gestionObjetosRouter = require('./routes/gestion/gestionObjetos')
-main().then(()=> storageSize().then(doc => console.log(doc)))
+const gestionArrysRouter = require('./routes/gestion/gestionArrys')
+main()//.then(()=> storageSize().then(doc => console.log(doc)))
 
-app.use(cors(corsOptions))
+app.use(cors({origin: '*'}))
 app.use(bodyParser())
 app.use("/",loginUsuarioRouter)
 app.use('/usuarios', gestionUsuariosRouter)
 app.use('/entradas', gestionEntradasRouter)
 app.use('/objetos', gestionObjetosRouter)
+app.use('/arrys', gestionArrysRouter)
 app.listen(3000, ()=>{
   console.log('El servidor esta corriendo por el puerto 3000')
   console.log('⭕ Cargando...')
